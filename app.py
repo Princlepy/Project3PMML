5b2df9c6f8693f3bd0520bba04287c52886456f3
Princlepy <135141718+Princlepy@users.noreply.github.com>
2025-06-04 16:48:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ FEATURE_ORDER = [
     'cat__browser_type_Firefox', 'cat__browser_type_Edge', 'cat__browser_type_Safari',
     'cat__browser_type_Unknown', 'cat__protocol_type_TCP', 'cat__protocol_type_UDP',
     'cat__protocol_type_ICMP', 'cat__encryption_used_AES', 'cat__encryption_used_DES',
-    'cat__encryption_used_None'
+    'cat__encryption_used_None', '17', '18', '19', '20'
 ]
 
 ORIGINAL_NUMERIC_FEATURES = [
