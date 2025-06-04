4ce2667c29acfb43b6f083d0d9b86d2e3e821c6f
Princlepy <135141718+Princlepy@users.noreply.github.com>
2025-06-04 16:34:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import pandas as pd
 import joblib
 import numpy as np
 import os
-# Untuk grafik tambahan, mungkin perlu import:
-import matplotlib.pyplot as plt
-import seaborn as sns
 import datetime # Tidak digunakan secara langsung di bagian ini, tapi bisa relevan untuk logging
 
 # ======================================================================================
